b65de579609eecf56367b7083cd4097bc5fb3a57
Marcel Müller <neikos@neikos.email>
2023-06-02 14:56:59 +0200

    Make error public
    Signed-off-by: Marcel Müller <neikos@neikos.email>
    (cherry picked from commit 8460b4b669384d3fb1b8805d2628a406de8c514d)

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,20 @@
+/// An error happened during deserialization
 #[derive(Debug, PartialEq, Eq, thiserror::Error)]
 pub enum EnvDeserializationError {
+    /// An error occurred during deserialization with serde
     #[error("An error occured during deserialization: {}", .0)]
     GenericDeserialization(String),
+
+    /// An unsupported variant was tried to be deserialized. Only structs and maps are currently
+    /// supported.
     #[error("An unsupported variant was tried to be deserialized. Only structs and maps are currently supported.")]
     UnsupportedValue,
+
+    /// An invalid nested value was given. (Usually only a simple value is allowed)
     #[error("Tried to nest values while a simple value was expected")]
     InvalidNestedValues,
+
+    /// Invalid nesting detected for the given paths ending in the given array
     #[error("Invalid nesting detected for paths ending in: {:?}", .0)]
     InvalidEnvNesting(Vec<String>),
 }
